d98e181f4bb83d73419a38ac652563c897d90fde
rml1168 <rml1168@rit.edu>
2019-10-23 20:59:24 -0400

    Basic documentation

--- BotIO.py
+++ BotIO.py
@@ -1,13 +1,25 @@
-# Work with Python 3.6
+"""
+file: BotIO.py
+purpose: Handles IO from server text channels to bot and vice versa
+author: rml1168@rit.edu (Ryan Lei)
+author: jdesig8@aol.com (Jordan Disciglio)
+"""
 from discord.ext.commands import Bot
 
+# Bot token
 TOKEN = 'NjM2NzA2NzA0OTkzNTUwMzM2.XbDiYg.hA69bZEhxZRxHiP7T78RdqSWaYs'
 
+# Bot prefix
 BOT_PREFIX = "!"
 client = Bot(command_prefix=BOT_PREFIX)
 
 @client.event
 async def on_message(message):
+    """
+    What the bot will do when a message is received
+    :param message: Represents a message from discord
+    :return:
+    """
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
@@ -18,6 +30,10 @@ async def on_message(message):
 
 @client.event
 async def on_ready():
+    """
+    What the bot does on launch
+    :return:
+    """
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
